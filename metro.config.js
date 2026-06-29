f15e720b61e577778de8df054e2c791e2aa23584
segz7448 <segz7448@gmail.com>
2026-06-29 05:04:50 -0700

    fix: metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,13 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
-const config = {};
+/**
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
+ */
+const config = {
+  resolver: {
+    assetExts: ['bin', 'txt', 'jpg', 'png', 'jpeg', 'gif', 'webp', 'ttf', 'otf', 'woff', 'woff2'],
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
