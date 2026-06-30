c608dee0cb36d26233fdbe2dd0730adc96d30d4e
segz7448 <segz7448@gmail.com>
2026-06-29 21:40:02 -0700

    fix: index.js

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 /**
  * MobileCloud - React Native App
+ * @format
  */
-
-// Polyfills required for Supabase + React Native
-import 'react-native-url-polyfill/auto';
-
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
