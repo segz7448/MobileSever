7988cdaa0e085b9b74b8e8384b25b657471eac82
segz7448 <segz7448@gmail.com>
2026-07-03 14:37:01 -0700

    fix crash: android/app/src/main/java/com/mobilecloud/MainActivity.kt

--- android/app/src/main/java/com/mobilecloud/MainActivity.kt
+++ android/app/src/main/java/com/mobilecloud/MainActivity.kt
@@ -1,26 +1,18 @@
 package com.mobilecloud
 
-import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
+
     override fun getMainComponentName(): String = "MobileCloud"
 
     override fun createReactActivityDelegate(): ReactActivityDelegate =
-        DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+        DefaultReactActivityDelegate(this, mainComponentName, false)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val serviceIntent = Intent(this, CloudBackgroundService::class.java)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            startForegroundService(serviceIntent)
-        } else {
-            startService(serviceIntent)
-        }
     }
 }
