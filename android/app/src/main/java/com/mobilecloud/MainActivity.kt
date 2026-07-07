bddab217e3d4473fdc39df9abd27f3946f71caf8
segz7448 <segz7448@gmail.com>
2026-07-06 21:07:32 -0700

    fresh: android/app/src/main/java/com/mobilecloud/MainActivity.kt

---
+++ android/app/src/main/java/com/mobilecloud/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.mobilecloud
+
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+
+class MainActivity : ReactActivity() {
+
+  override fun getMainComponentName(): String = "MobileCloud"
+
+  override fun createReactActivityDelegate(): ReactActivityDelegate =
+      DefaultReactActivityDelegate(this, mainComponentName, false)
+}
